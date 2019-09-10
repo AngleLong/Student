cc7e17e7f0a1cf26138230238123f0ffd39e5457
贺金龙 <hejinlong@hejinlongdeMacBook-Pro.local>
2019-09-10 20:08:36 +0800

    feat:添加了ConstraintSet动画的实现

--- app/src/main/java/com/angle/student/MainActivity.java
+++ app/src/main/java/com/angle/student/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.angle.view.AnimationViewActivity;
 import com.angle.view.ConstraintLayoutActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void alarmClick(View view) {
-        Intent intent = new Intent(this, ConstraintLayoutActivity.class);
+        Intent intent = new Intent(this, AnimationViewActivity.class);
         startActivity(intent);
     }
 }
--- view/src/main/java/com/angle/view/ConstraintLayoutActivity.java
+++ view/src/main/java/com/angle/view/ConstraintLayoutActivity.java
@@ -1,5 +1,7 @@
 package com.angle.view;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.transition.TransitionManager;
 import android.view.View;
@@ -15,6 +17,7 @@ import androidx.constraintlayout.widget.ConstraintSet;
 public class ConstraintLayoutActivity extends AppCompatActivity {
 
     private ConstraintLayout rootCL;
+    private Button mButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +26,48 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
 
         rootCL = findViewById(R.id.rootCL);
 
+        //添加一个按钮
+        addView();
+        initListener();
+    }
+
+    public void addView() {
         //动态布局添加一个顶部的按钮
         //1. 创建一个 ConstraintSet 对象用于构建
         ConstraintSet constraintSet = new ConstraintSet();
         //2. 构建一个Button对象
-        Button button1 = new Button(this);
-        button1.setId(View.generateViewId());
-        button1.setText("动态添加的第一个按钮");
+        mButton1 = new Button(this);
+        //这个是一个17的方法
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            mButton1.setId(View.generateViewId());
+        } else {
+            mButton1.setId(R.id.addButton);
+        }
+        mButton1.setText("跳转到控件的动画页面");
         //3. 添加一个对象
-        rootCL.addView(button1);
+        rootCL.addView(mButton1);
         //4. 克隆一个ConstraintLayout
         constraintSet.clone(rootCL);
+        //下面的注释和上面是一样的
+        //constraintSet.clone(this,R.layout.activity_constraint_layout);
         //5. 添加相应的约束
         //宽度全屏
-        constraintSet.constrainWidth(button1.getId(), ConstraintLayout.LayoutParams.MATCH_PARENT);
+        constraintSet.constrainWidth(mButton1.getId(), ConstraintLayout.LayoutParams.MATCH_PARENT);
         //高度包裹内容
-        constraintSet.constrainHeight(button1.getId(), ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        constraintSet.constrainHeight(mButton1.getId(), ConstraintLayout.LayoutParams.WRAP_CONTENT);
         //相应的约束
-        constraintSet.connect(button1.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
-        constraintSet.connect(button1.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT);
-        constraintSet.connect(button1.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+        constraintSet.connect(mButton1.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        constraintSet.connect(mButton1.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT);
+        constraintSet.connect(mButton1.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
         constraintSet.applyTo(rootCL);
+    }
 
-        TransitionManager.
+    private void initListener() {
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ConstraintLayoutActivity.this,AnimationActivity.class));
+            }
+        });
     }
 }
