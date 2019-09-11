f552de8372b55c1e50036733657cc123d15ce464
贺金龙 <hejl@bjrrtx.com>
2019-09-11 14:56:19 +0800

    feat:添加了非对称签名算法

--- java/src/main/java/com/angle/java/DesActivity.java
+++ java/src/main/java/com/angle/java/DesActivity.java
@@ -5,16 +5,12 @@ import android.util.Base64;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 
 public class DesActivity extends AppCompatActivity {
 
@@ -23,71 +19,27 @@ public class DesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_des);
 
-        //加密
-        byte[] encryption = encryption("helloWord");
-        byte[] encodeBase64 = Base64.encode(encryption, Base64.DEFAULT);
-        String encrypStr = new String(encodeBase64);
-        System.out.println("加密后的数据为===》" + encrypStr);
-        byte[] decrypt = decrypt(encrypStr);
-        byte[] decodeStr = Base64.decode(decrypt, Base64.DEFAULT);
-        System.out.println("解密后的数据为===》" + new String(decodeStr));
-    }
-
-    /**
-     * 加密
-     *
-     * @param encryptionStr 加密字符串
-     * @return 加密后的字符串
-     */
-    public static byte[] encryption(String encryptionStr) {
-        //1,得到cipher 对象（可翻译为密码器或密码系统）
+        String content = "Hello Word";
         try {
-            Cipher cipher = Cipher.getInstance("DES");
-            //2，创建秘钥
-            SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-            //3，设置操作模式（加密/解密）
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            //4，执行操作
-            return cipher.doFinal(encryptionStr.getBytes("utf-8"));
-        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
+            //1，获取cipher 对象
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            //2，通过秘钥对生成器KeyPairGenerator 生成公钥和私钥
+            KeyPair keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+            //使用公钥进行加密，私钥进行解密（也可以反过来使用）
+            PublicKey publicKey = keyPair.getPublic();
+            System.out.println(Base64.encodeToString(publicKey.getEncoded(), Base64.DEFAULT));
+            PrivateKey privateKey = keyPair.getPrivate();
+            //3,使用公钥初始化密码器
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            //4，执行加密操作
+            byte[] result = cipher.doFinal(content.getBytes());
+            //使用私钥初始化密码器
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            //执行解密操作
+            byte[] deResult = cipher.doFinal(result);
+            System.out.println(new String(deResult));
+        } catch (Throwable e) {
             e.printStackTrace();
         }
-        return null;
-    }
-
-    /**
-     * 解密
-     *
-     * @param decryptStr 待解密的字符串
-     * @return 加密后的字符串
-     */
-    public static byte[] decrypt(String decryptStr) {
-        //1,得到cipher 对象（可翻译为密码器或密码系统）
-        try {
-            Cipher cipher = Cipher.getInstance("DES");
-            //2，创建秘钥
-            SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-            //3，设置操作模式（加密/解密）
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            //4，执行操作
-            return cipher.doFinal(decryptStr.getBytes("utf-8"));
-        } catch (NoSuchAlgorithmException | InvalidKeyException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
